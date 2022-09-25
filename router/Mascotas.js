c8651b76b72422537f3dcbf70830d31fa2b9a5a5
juanjo <juanjoibar@gmail.com>
2022-09-25 18:46:19 -0300

    second commit

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -1,5 +1,6 @@
 
 const Mascota = require('../models/mascota')
+
 router.get('/', async(req,res)=>{
     try {
         const arrayMascotasDB = await Mascotas.find()
@@ -12,3 +13,42 @@ router.get('/', async(req,res)=>{
         console.log(error)
     }
 }))
+
+router.get('/crear',(req,res)=>{
+    res.render('crear')
+})
+router.post('/guardar',(req,res)=>{
+    const body = req.body
+    console.log(body)
+    try {
+         const mascotaDB = new Mascota(body);
+         await mascotaDB.save()
+         console.log('Mascota creada:'+mascotaDB)
+         res.redirect('/mascotas');
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.get('/:id',async(req,res)=>{
+    const id = req.param.id
+    try {
+        const mascotaDB = await Mascota.findOne({  _id:id})
+        console.log(mascotaDB);
+        res.render('detalle',{
+            mascota: mascotaDB,
+            error: false
+
+        })
+    } catch (error) {
+        console.log(error);
+        res.render('detalle',{
+            error: true,
+            mensaje: 'no se encontro la mascota'
+
+
+        })
+
+    }
+})
+module.exports = router;
